c88a16c2956eefe6d477732f2c4fb6cc5b904487
Jean Diaz <djjean85@gmail.com>
2018-04-10 22:13:34 -0700

    added fetch data and WP setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import { BrowserRouter, NavLink, Route,  } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+let Router = BrowserRouter;
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: "hey"
+    }
+  }
+
+componentDidMount() {
+  fetch("http://amazonmug.local/wp-json/wp/v2/posts/1")
+	  .then(response => response.json())
+	  .then(
+	  data => {
+		  this.setState({posts: data});
+		  console.log(this.state.posts);
+		}
+	);
+ 
+}
+
+
   render() {
     return (
+      <Router>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+			{this.state.posts.date}
         </p>
       </div>
+      </Router>
     );
   }
 }
