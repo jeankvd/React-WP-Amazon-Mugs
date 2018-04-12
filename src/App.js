457d8982ab109838c00985d81bcfa4bf0282131f
Jean Diaz <djjean85@gmail.com>
2018-04-11 20:47:46 -0700

    Added loop through mugs

--- src/App.js
+++ src/App.js
@@ -4,23 +4,65 @@ import logo from './logo.svg';
 import './App.css';
 let Router = BrowserRouter;
 
+{/* <h1>{mugInfo.title.rendered}</h1>
+<p>{mugInfo.content.rendered}</p>
+<a href={mugInfo.acf.link_to_mug}>Buy It Here!</a> */}
+
+
+let Mug = (props) => {
+  let mugInfo = props.mugInformation;
+
+  if (!props.loaded) {
+    return (
+      <h1>loading</h1>
+    )
+  } else {
+    return (
+      <NavLink to="">
+        <div className="mug">
+          <img src={mugInfo._embedded["wp:featuredmedia"][0].source_url} alt=""/>
+        </div>
+      </NavLink>
+    ) 
+}
+}
+
+let Mugs = (props) => {
+  let mugs = [];
+  for (let i = 0; i < props.posts.length; i++) {
+    mugs.push(<Mug key={i} mugInformation={props.posts[i]} loaded={props.loaded} />);
+  }
+  return (
+    <div>
+      {mugs.map(function(listValue){
+		 return listValue;
+		})}
+    </div>
+  )
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: "hey"
+      posts: {},
+      loaded: false
     }
   }
 
 componentDidMount() {
-  fetch("http://amazonmug.local/wp-json/wp/v2/posts/1")
+  fetch("http://amazonmug.local/wp-json/wp/v2/mugs?_embed")
 	  .then(response => response.json())
 	  .then(
 	  data => {
 		  this.setState({posts: data});
 		  console.log(this.state.posts);
 		}
-	);
+	).then(
+    data => {
+      this.setState({loaded: true})
+    }
+  );
  
 }
 
@@ -28,14 +70,13 @@ componentDidMount() {
   render() {
     return (
       <Router>
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-			{this.state.posts.date}
-        </p>
-      </div>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">Welcome to React</p>
+          <Mugs posts={this.state.posts} loaded={this.state.loaded} />
+        </div>
       </Router>
     );
   }
