f00c143db57b04b16c905c65d063375679178942
Jean Diaz <djjean85@gmail.com>
2018-04-11 21:45:36 -0700

    Added Route to Complete Mug Component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import logo from './logo.svg';
 import './App.css';
 let Router = BrowserRouter;
 
-{/* <h1>{mugInfo.title.rendered}</h1>
-<p>{mugInfo.content.rendered}</p>
-<a href={mugInfo.acf.link_to_mug}>Buy It Here!</a> */}
-
 
 let Mug = (props) => {
   let mugInfo = props.mugInformation;
@@ -18,7 +14,7 @@ let Mug = (props) => {
     )
   } else {
     return (
-      <NavLink to="">
+      <NavLink to={"/mugs/:" + props.mugNumber}>
         <div className="mug">
           <img src={mugInfo._embedded["wp:featuredmedia"][0].source_url} alt=""/>
         </div>
@@ -30,7 +26,7 @@ let Mug = (props) => {
 let Mugs = (props) => {
   let mugs = [];
   for (let i = 0; i < props.posts.length; i++) {
-    mugs.push(<Mug key={i} mugInformation={props.posts[i]} loaded={props.loaded} />);
+    mugs.push(<Mug key={i} mugInformation={props.posts[i]} mugNumber={i} loaded={props.loaded} />);
   }
   return (
     <div>
@@ -40,6 +36,8 @@ let Mugs = (props) => {
     </div>
   )
 }
+
+
 
 class App extends Component {
   constructor(props) {
@@ -63,7 +61,6 @@ componentDidMount() {
       this.setState({loaded: true})
     }
   );
- 
 }
 
 
@@ -76,9 +73,33 @@ componentDidMount() {
           </header>
           <p className="App-intro">Welcome to React</p>
           <Mugs posts={this.state.posts} loaded={this.state.loaded} />
+            <div className="singleMug">
+            <Route path="/mugs/:id" render={(props) => (<MugPage mugNumber={props.match.params.id.substring(1)} mugInformation={this.state.posts[props.match.params.id.substring(1)]} loaded={this.state.loaded} {...props}/>)} />
+          </div>
         </div>
       </Router>
     );
+  }
+}
+
+let MugPage = (props) => {
+  let mugInfo = props.mugInformation;
+
+  if (!props.loaded) {
+    return (
+      <h1>loading</h1>
+    )
+  } else {
+    return (
+      <NavLink to={"/mugs/:" + props.mugNumber}>
+        <div className="mug">
+          <img src={mugInfo._embedded["wp:featuredmedia"][0].source_url} alt=""/>
+          <h1>{mugInfo.title.rendered}</h1>
+          <p>{mugInfo.content.rendered}</p>
+          <a href={mugInfo.acf.link_to_mug}>Buy It Here!</a>
+        </div>
+      </NavLink>
+    ) 
   }
 }
 
